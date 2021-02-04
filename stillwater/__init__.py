2d42945dc05407a1bbb39cc368f45fd738eab59a
Alec Gunny <alec.gunny@gmail.com>
2021-02-04 13:52:56 -0800

    updating init

--- stillwater/__init__.py
+++ stillwater/__init__.py
@@ -1,4 +1,4 @@
 from .client import StreamingInferenceClient
 from .data_generator import *
 from .streaming_inference_process import StreamingInferenceProcess
-from .utils import ExceptionWrapper
+from .utils import ExceptionWrapper, pipe
