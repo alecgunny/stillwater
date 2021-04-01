5d290ecdd9a1f88ea188eb69d0f62e190bf2b71e
Alec Gunny <alec.gunny@gmail.com>
2021-04-01 09:01:44 -0700

    trying data generator as process again

--- stillwater/data_generator/dummy_data_generator.py
+++ stillwater/data_generator/dummy_data_generator.py
@@ -1,10 +1,45 @@
 import time
 import typing
+from multiprocessing import Event, Process, Queue
+from queue import Empty
 
 import numpy as np
 
 from stillwater.data_generator import DataGenerator
-from stillwater.utils import Package, gps_time
+from stillwater.utils import ExceptionWrapper, Package, gps_time
+
+
+class Stopped(Exception):
+    pass
+
+
+def generate(q, stop_event, shape, generation_rate):
+    if generation_rate is not None:
+        _sleep_time = 1 / generation_rate - 4e-4
+    else:
+        _sleep_time = None
+
+    try:
+        last_time = time.time()
+        while not stop_event.is_set():
+            if _sleep_time is not None:
+                while (time.time() - last_time) < _sleep_time:
+                    time.sleep(1e-6)
+
+            x = np.random.randn(*shape).astype("float32")
+            package = Package(x=x, t0=gps_time())
+            last_time = package.t0
+
+            while q.full():
+                if stop_event.is_set():
+                    raise Stopped
+                time.sleep(1e-6)
+            q.put(x)
+
+    except Stopped:
+        pass
+    except Exception as e:
+        q.put(ExceptionWrapper(e))
 
 
 class DummyDataGenerator(DataGenerator):
@@ -15,19 +50,31 @@ class DummyDataGenerator(DataGenerator):
         generation_rate: typing.Optional[float] = None,
     ) -> None:
         super().__init__(name=name)
-        self.shape = shape
-        if generation_rate is not None:
-            self._sleep_time = 1 / generation_rate - 4e-4
-        else:
-            self._sleep_time = None
-        self._last_time = time.time()
+        self._q = Queue(maxsize=100)
+        self._stop_event = Event()
+        self._p = Process(
+            target=generate,
+            args=(self._q, self._stop_event, shape, generation_rate),
+        )
+
+    def __iter__(self):
+        self._p.start()
+        return self
 
     def __next__(self):
-        if self._sleep_time is not None:
-            while (time.time() - self._last_time) < self._sleep_time:
-                time.sleep(1e-6)
+        try:
+            package = self._q.get(timeout=1)
+        except Empty:
+            raise RuntimeError("Nothing generating")
 
-        x = np.random.randn(*self.shape).astype("float32")
-        package = Package(x=x, t0=gps_time())
-        self._last_time = package.t0
+        if isinstance(package, ExceptionWrapper):
+            package.reraise()
         return package
+
+    def stop(self):
+        self._stop_event.set()
+        self._p.join(0.5)
+        try:
+            self._p.close()
+        except ValueError:
+            self._p.terminate()
